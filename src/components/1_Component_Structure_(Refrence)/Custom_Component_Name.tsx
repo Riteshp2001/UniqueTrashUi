57e6e49b31de29fc1b7824a1d0c93fbb71743b36
PANDIT <87899750+Riteshp2001@users.noreply.github.com>
2024-02-10 14:49:23 +0530

    Somne minor changes and Bugfixes

--- src/components/1_Component_Structure_(Refrence)/Custom_Component_Name.tsx
+++ src/components/1_Component_Structure_(Refrence)/Custom_Component_Name.tsx
@@ -1,9 +1,5 @@
-import React from 'react'
-
 const Custom_Component_Name = () => {
-  return (
-    <div>Custom_Component_Name</div>
-  )
-}
+	return <div>Custom_Component_Name</div>;
+};
 
-export default Custom_Component_Name
+export default Custom_Component_Name;
---
+++ src/components/1_Component_Structure_(Refrence)/common_exports.js
@@ -0,0 +1,4 @@
+//Common exports of the whole Folder project
+
+//exporting all the components
+// ex: export { default as Header } from './Header';
